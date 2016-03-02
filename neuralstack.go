363e57bafb76c507408df6e38dc1adabebebe4ce
Glen Baker <iepathos@gmail.com>
2016-03-02 16:25:00 -0500

    Fixed NeuralStack s_t, changed NeuralStack.s to 2d array

--- neuralstack.go
+++ neuralstack.go
@@ -1,17 +1,13 @@
 package main
 
-import (
-// "fmt"
-)
-
 type NeuralStack struct {
 	stackWidth  int
 	o_prime_dim int
 
-	V []float64 // stack states
-	s []float64 // stack strengths
-	d []float64 // push strengths
-	u []float64 // pop strengths
+	V []float64   // stack states
+	s [][]float64 // stack strengths
+	d []float64   // push strengths
+	u []float64   // pop strengths
 	r []float64
 	o []float64
 
@@ -24,31 +20,30 @@ type NeuralStack struct {
 }
 
 func (ns *NeuralStack) reset() {
-	ns.V = []float64{0.0}
-	ns.s = []float64{0.0}
-	ns.d = []float64{0.0}
-	ns.u = []float64{0.0}
-	ns.r = []float64{0.0}
-	ns.o = []float64{0.0}
+	ns.V = []float64{}
+	ns.s = [][]float64{}
+	ns.d = []float64{}
+	ns.u = []float64{}
+	ns.r = []float64{}
+	ns.o = []float64{}
 
-	ns.V_delta = []float64{0.0}
-	ns.s_delta = []float64{0.0}
-	ns.d_error = []float64{0.0}
-	ns.u_error = []float64{0.0}
+	ns.V_delta = []float64{}
+	ns.s_delta = []float64{}
+	ns.d_error = []float64{}
+	ns.u_error = []float64{}
 	ns.t = 0
 }
 
-// func (ns *NeuralStack) s_t(i int) {
-// 	if i >= 0 && i < ns.t {
-// 		inner_sum := ns.s[ns.t-1][i+1 : ns.t-0]
-// 		return Relu(ns.s[ns.t-1][i] - Relu(ns.u[ns.t]-Sum(inner_sum)))
-// 	} else if i == ns.t {
-// 		return ns.d[ns.t]
-// 	} else {
-// 		fmt.Println("Problem")
-// 	}
-
-// }
+func (ns *NeuralStack) s_t(i int) float64 {
+	if i >= 0 && i < ns.t {
+		inner_sum := ns.s[ns.t-1][i+1 : ns.t-0]
+		return Relu(ns.s[ns.t-1][i]-Relu(ns.u[ns.t]-Sum(inner_sum), false), false)
+	} else if i == ns.t {
+		return ns.d[ns.t]
+	} else {
+		panic("An error is happening in NeuralStack.s_t")
+	}
+}
 
 // func (ns *NeuralStack) pushAndPopForward(v_t, d_t, u_t float64) {
 // 	ns.d = Append(ns.d, d_t)
