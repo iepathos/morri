ca67e2808e4436758b02cbd9c3eea48c7eac53ae
Glen Baker <iepathos@gmail.com>
2016-03-03 10:34:56 -0500

    Changed named to use golang camelcase naming

--- neuralstack.go
+++ neuralstack.go
@@ -1,8 +1,8 @@
 package main
 
 type NeuralStack struct {
-	stackWidth  int
-	o_prime_dim int
+	stackWidth int
+	oPrimeDim  int
 
 	V []float64   // stack states
 	s [][]float64 // stack strengths
@@ -11,10 +11,10 @@ type NeuralStack struct {
 	r []float64
 	o []float64
 
-	V_delta []float64 // stack states
-	s_delta []float64 // stack strengths
-	d_error []float64 // push strengths
-	u_error []float64 // pop strengths
+	VDelta []float64 // stack states
+	sDelta []float64 // stack strengths
+	dError []float64 // push strengths
+	uError []float64 // pop strengths
 
 	t int
 }
@@ -27,35 +27,35 @@ func (ns *NeuralStack) reset() {
 	ns.r = []float64{}
 	ns.o = []float64{}
 
-	ns.V_delta = []float64{}
-	ns.s_delta = []float64{}
-	ns.d_error = []float64{}
-	ns.u_error = []float64{}
+	ns.VDelta = []float64{}
+	ns.sDelta = []float64{}
+	ns.dError = []float64{}
+	ns.uError = []float64{}
 	ns.t = 0
 }
 
-func (ns *NeuralStack) s_t(i int) float64 {
+func (ns *NeuralStack) sT(i int) float64 {
 	if i >= 0 && i < ns.t {
 		innerSum := ns.s[ns.t-1][i+1 : ns.t-0]
 		return Relu(ns.s[ns.t-1][i]-Relu(ns.u[ns.t]-Sum(innerSum), false), false)
 	} else if i == ns.t {
 		return ns.d[ns.t]
 	} else {
-		panic("An error is happening in NeuralStack.s_t")
+		panic("An error is happening in NeuralStack.sT")
 	}
 }
 
-func (ns *NeuralStack) pushAndPopForward(v_t, d_t, u_t float64) {
-	ns.d = Append(ns.d, d_t)
-	ns.d_error = Append(ns.d_error, 0)
+func (ns *NeuralStack) pushAndPopForward(vT, dT, uT float64) {
+	ns.d = Append(ns.d, dT)
+	ns.dError = Append(ns.dError, 0)
 
-	ns.u = Append(ns.u, u_t)
-	ns.u_error = Append(ns.u_error, 0)
+	ns.u = Append(ns.u, uT)
+	ns.uError = Append(ns.uError, 0)
 
-	new_s := make([]float64, ns.t+1)
+	newS := make([]float64, ns.t+1)
 	for i := 0; i < ns.t+1; i++ {
-		new_s[i] = ns.s_t(i)
+		newS[i] = ns.sT(i)
 	}
-	ns.s = append(ns.s, new_s)
-	// ns.s_delta = append(np.zeros_like(new_s))
+	ns.s = append(ns.s, newS)
+	// ns.sDelta = append(np.zeros_like(new_s))
 }
